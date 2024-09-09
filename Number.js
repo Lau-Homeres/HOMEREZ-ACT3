39a6feb84844c274239178c9b12f9b3f50e72759
Lau Homeres <laurencehomeres0@gmail.com>
2024-09-09 23:15:40 +0800

    initial commit

---
+++ Number.js
@@ -0,0 +1,29 @@
+let fnum = 18
+let snum = 2
+let sum = fnum + snum;
+
+console.log("My favorite number is ", fnum, "\n" );
+console.log("My second favorite number is ", snum, "\n");
+
+console.log("-Using Addition- \n");
+console.log("18 + 2 = ", sum, "\n");
+
+console.log("-Using Subtraction- \n");
+let diff = fnum - snum;
+console.log("18 - 2 = ", sum, "\n");
+
+console.log("-Using Multiplication- \n");
+let prod = fnum * snum;
+console.log("18 * 2 = ", prod, "\n");
+
+console.log("-Using Division- \n");
+let quot = fnum / snum;
+console.log("18 / 2 = ", quot, "\n");
+
+console.log("-Using Modulus- \n");
+let mod = fnum % snum;
+console.log("18 % 2 = ",mod , "\n");
+
+console.log("-Using Exponentiation- \n");
+let expo = fnum ** snum;
+console.log("18^2 = ",expo , "\n");
